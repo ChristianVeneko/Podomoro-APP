65d8e961ef503a9cf5898b85be64f4f460b624c9
Christian Mendoza <christianmendozater@gmail.com>
2023-01-13 00:34:26 -0400

    Base app created

---
+++ app.js
@@ -0,0 +1,56 @@
+let timerDisplay = document.querySelector("#timer");
+let startBtn = document.querySelector("#start");
+let stopBtn = document.querySelector("#stop");
+let resetBtn = document.querySelector("#reset");
+let alarm = document.querySelector("#alarm");
+
+
+let timer = null;
+let defaultSeconds= 20
+let seconds = 20; // 25 minutes
+
+startBtn.addEventListener("click", startTimer);
+stopBtn.addEventListener("click", stopTimer);
+resetBtn.addEventListener("click", resetTimer);
+
+function startTimer() {
+  timer = setInterval(() => {
+    seconds--;
+    updateTimerDisplay();
+
+    if (seconds === 0) {
+      stopTimer();
+      alert("¡El tiempo ha terminado!");
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timer);
+}
+
+function resetTimer() {
+  stopTimer();
+  seconds = defaultSeconds;
+  updateTimerDisplay();
+}
+
+function updateTimerDisplay() {
+    let minutes = Math.floor(seconds / 60);
+    let remainderSeconds = seconds % 60;
+    timerDisplay.textContent = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`;
+    document.title = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds} - Pomodoro APP`;
+}
+
+function startTimer() {
+  timer = setInterval(() => {
+    seconds--;
+    updateTimerDisplay();
+
+    if (seconds === 0) {
+      stopTimer();
+      alarm.play();
+      resetTimer()
+    }
+  }, 1000);
+}
